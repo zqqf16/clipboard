e17b319b59e91bdbcd89fe00d67d396b5cb946e1
zqqf16 <zqqf16@gmail.com>
2013-12-11 05:56:17 -0800

    Fix issue #2 * Using Sqlite to replace JSON-DB * Using peewee for ORM

--- clipboard/handler.py
+++ clipboard/handler.py
@@ -2,20 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import tornado.web
-from model import Message, datetime
+from model import Message
+from datetime import datetime
 
 class Clipboard(tornado.web.RequestHandler):
     def get(self):
-        msgs = Message.all()
+        msgs = [m for m in Message.select().order_by(Message.date.desc())]
         msgs.sort(lambda x, y: cmp(x.date, y.date), reverse=True)
         self.render('clipboard.html', messages=msgs)
 
     def post(self):
         content = self.get_argument('content', None)
         date = datetime.now()
-        m = Message(content=content, date=date)
-
-        Message.add(m)
-        Message.save()
-
+        m = Message.create(content=content, date=date)
+        m.save()
         self.redirect('/clipboard')
